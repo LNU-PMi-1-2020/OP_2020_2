c87f00b4575f71ed39d156fd4a30b3d1b2768035
khiletska <hiletskak@gmail.com>
2020-10-19 22:50:51 +0300

    add skeleton

---
+++ tasks/task01/khiletska/проект.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a, b, c, x, x1, x2;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    if (a == 0)
+        cout << " not a quadratic equation";
+    else
+    {
+        double d = b * b - 4 * a * c;
+        if (d < 0)
+            cout << "no solution";
+        else if (d == 0)
+        {
+            x = (-b) / (2 * a);
+            cout << x;
+        }
+        else
+        {
+            x1 = (-b + sqrt(d)) / (2 * a);
+            x2 = (-b - sqrt(d)) / (2 * a);
+            cout << x1 << " " << x2;
+        }
+
+    }
+  
+}
+
